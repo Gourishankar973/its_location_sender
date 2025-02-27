6f34a434b602a38e4cb773da4cfd5963284f2681
Gourishankar A J <21ad212@mgits.ac.in>
2025-02-27 22:13:16 +0530

    hopefully this sender works properly

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ def update_bus_location():
     bus_id = data.get("bus_id")
     lat = data.get("lat")
     lon = data.get("lon")
-    #print(lon, lat)
     
     if not all([user_id, bus_id, lat, lon]):
         return jsonify({"error": "Missing data"}), 400
@@ -29,12 +28,12 @@ def update_bus_location():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# New route to stop tracking and delete records for a bus_id.
+# Route to stop tracking and delete records for a bus_id.
 @app.route('/stop_tracking', methods=['POST'])
 def stop_tracking():
     data = request.get_json()
     bus_id = data.get("bus_id")
-    print(bus_id,'pk')
+    print(bus_id, 'pk')
     if not bus_id:
         return jsonify({"error": "Missing bus_id"}), 400
     try:
